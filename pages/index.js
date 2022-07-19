079bee1dffbb18469be53a49d1bac9cf9c9608ee
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-07-19 17:31:08 +0530

    connected moralis server to the frontend app

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>NFT Marketplace</title>
-        <meta name="description" content="Welcome to the NFT Marketplace" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      Hi
-    </div>
-  )
+    //How do we show the recently listed NFTs?
+
+    // we will index the events off-chain and then read from our database.
+    // Setup a server to listen for those events to be fired, and we will add them to a database to query.
+    // Isn't that centralized? No necessarily --> GraphQL is the answer.
+
+    //The Graph does it in a decentralized way.
+    //Moralis does it in a centralized way and comes with a ton of the features.
+
+    // All our logic is still 100% on chain.
+    // speed & development time
+    // Its really hard to start a pro blockchain project 100% decentralized.
+    // They are working open sourcing their code.
+    // Feature Richness
+
+    
+    return <div className={styles.container}>Hi</div>
 }
