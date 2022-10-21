5c5f6f4aaa53783ea96765ba3f7e925dd9fe4ad5
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-10-21 23:39:43 +0530

    moved environment variables to next.config.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ import Header from "../components/Header"
 import Head from "next/head"
 import { NotificationProvider } from "web3uikit"
 
-const APP_ID = process.env.NEXT_PUBLIC_APP_ID
-const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+const APP_ID = process.env.APP_ID
+const SERVER_URL = process.env.SERVER_URL
 
 function MyApp({ Component, pageProps }) {
     return (
